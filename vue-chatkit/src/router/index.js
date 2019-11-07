a9969acdb9d9340f777e43fede0bee9ee6f5c22d
zihuahuahua <839711063@qq.com>
2019-11-07 15:22:56 +0800

    fix: login页面

--- vue-chatkit/src/router/index.js
+++ vue-chatkit/src/router/index.js
@@ -1,22 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import ChatDashboard from '../views/ChatDashboard.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    name: 'login',
+    component: Login
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/chat',
+    name: 'chat',
+    component: ChatDashboard
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
--- vue-chatkit/src/store/index.js
+++ vue-chatkit/src/store/index.js
@@ -1,15 +1,36 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersistence from 'vuex-persist'
+import mutations from './mutations'
+import actions from './actions'
 
 Vue.use(Vuex)
 
+const debug = process.env.NODE_ENV !== 'production'
+
+// vuex-persist 确保在页面重新加载或刷新之间保存我们的Vuex状态
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage
+})
+
 export default new Vuex.Store({
   state: {
+    loading: false,
+    sending: false,
+    error: null,
+    user: [],
+    reconnect: false,
+    activeRoom: null,
+    rooms: [],
+    users: [],
+    messages: [],
+    userTyping: null
   },
-  mutations: {
+  mutations,
+  actions,
+  getters: {
+    hasError: state => state.error ? true : false
   },
-  actions: {
-  },
-  modules: {
-  }
+  plugins: [vuexLocal.plugin],
+  strict: debug
 })
